2ad519aca670650328175ece9659fc1f0ac048ec
GunawanKafi <kafiagungg@gmail.com>
2026-03-06 04:03:46 +0700

    penambahan materi

--- materi/script.js
+++ materi/script.js
@@ -39,25 +39,47 @@ const databaseMateri = {
     1: {
       // Semester 1
       uts: [
-        // {
-        //   pertemuan: 1,
-        //   judul: "VLAN (Virtual LAN)",
-        //   deskripsi: "Konfigurasi dasar VLAN pada Switch Cisco Packet Tracer.",
-        //   fileId: "GANTI_DENGAN_ID_GOOGLE_DRIVE_VLAN",
-        // },
+        {
+          pertemuan: 1,
+          judul: "Penjelasan Dasar Ibis Paint X",
+          deskripsi:
+            "Modul ini berfungsi sebagai panduan awal pengenalan antarmuka aplikasi Ibis Paint X. Materi mencakup langkah-langkah dasar mulai dari cara mengakses menu 'Galeri Saya' untuk melihat karya, prosedur pembuatan kanvas baru melalui tombol tambah, hingga pemahaman vital mengenai pemilihan ukuran kanvas (SD/HD) dan pengenalan tata letak toolbar utama yang digunakan dalam proses menggambar digital.",
+          fileId: "1s4aSmU5SK0e7FTyQq5aBW2_Bc2SXUW8N",
+        },
+        {
+          pertemuan: 2,
+          judul: "Penjelasan Kuas Ibis Paint X",
+          deskripsi:
+            "Materi ini membedah fitur terpenting dalam ilustrasi digital, yaitu pengaturan alat Kuas (Brush). Penjelasan difokuskan pada tata cara mengakses perpustakaan kuas, memilih jenis kuas yang tepat sesuai kebutuhan seperti Dip Pen (Hard) atau Felt Tip Pen (Soft), serta teknik kustomisasi ketebalan dan tingkat opasitas kuas untuk menciptakan variasi goresan yang dinamis dan berkarakter.",
+          fileId: "1taRWds8Lb4n1TYa7XLN7wdRgHJV51bXY",
+        },
+        {
+          pertemuan: 3,
+          judul: "Penjelasan Stabilizer Ibis Paint X",
+          deskripsi:
+            "Modul teknis ini mengajarkan cara menghasilkan Line Art yang halus dan profesional menggunakan fitur Stabilizer. Siswa akan mempelajari cara mengaktifkan alat bantu ini melalui ikon jari untuk meredam getaran tangan saat menarik garis, serta memanfaatkan fitur 'Paksa Pudar' (Force Fade) untuk mengatur panjang awal dan akhir goresan agar menghasilkan ujung garis yang lancip dan tajam.",
+          fileId: "18IVqbomi4SmGFlR_llRFwwlR3fZA1X0k",
+        },
       ],
       kisiUts: "", // Kosongkan ("") jika belum ada
 
       uas: [
-        // {
-        //   pertemuan: 8, // Lanjutan setelah UTS
-        //   judul: "Konsep IP Address v4",
-        //   deskripsi:
-        //     "Memahami struktur IP Address, kelas IP, dan konversi biner ke desimal.",
-        //   fileId: "GANTI_DENGAN_ID_GOOGLE_DRIVE_3",
-        // },
+        {
+          pertemuan: 4, // Lanjutan setelah UTS
+          judul: "Topologi Jaringan",
+          deskripsi:
+            "Disusun oleh Kelompok 1 (Fani, Azizah, Ratna), presentasi ini membahas secara spesifik mengenai arsitektur Topologi Jaringan. Materi mencakup analisis mendalam tentang karakteristik, kelebihan, serta kekurangan dari Topologi Ring yang membentuk sirkuit data tertutup, dan Topologi Mesh yang menawarkan redundansi tinggi melalui koneksi langsung antar perangkat. Pemahaman ini krusial untuk merancang jaringan yang efisien dan minim risiko kegagalan.",
+          fileId: "1K-n4zjMf1QrHl6hq7-XMQfiRns-M6Vcb",
+        },
+        {
+          pertemuan: 5, // Lanjutan setelah UTS
+          judul: "Pengenalan Hardware",
+          deskripsi:
+            "Kelompok 2 (Sopi, Emy, Rahma, Tasya) menyajikan materi fundamental mengenai Perangkat Keras (Hardware). Selain mendefinisikan hardware sebagai komponen fisik komputer yang dapat disentuh, modul ini secara khusus membedah fungsi vital dari Processor sebagai otak pemrosesan data instruksi, serta GPU (Graphics Processing Unit) yang bertugas khusus untuk merender grafik dan visual 2D/3D secara cepat dan efisien.",
+          fileId: "1AohWQTkGGFJsJsAD1gtU4D7iDt4IxmJg",
+        },
       ],
-      kisiUas: "",
+      kisiUas: "14CY_EYlmnKEdcEJyq2SEWc8lsibTXSh-",
     },
     2: {
       uts: [
